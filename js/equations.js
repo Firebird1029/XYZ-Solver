7c0807f205f385d98e3bafa1e3e07bd5d15a2d66
jason2020 <jtay20@punahou.edu>
2015-12-10 20:38:23 -1000

    Arrays and more error fixing
    I finally made it so that there is only one array. Now there is only
    one array to store the variables. I fixed up some more errors, leaving
    it with only 27 errors.

--- js/equations.js
+++ js/equations.js
@@ -42,107 +42,101 @@ function twoEquations (equation, equation2) {
 
 // the function for three equations
 function threeEquations (e1, e2, e3) {
-	var x, y, z;
 	var solve = function solve () {
-
-		// first remove one variable
-		var rem1 = [],
-			rem2 = [],
-			rem3 = [],
-
-		// repeat by removing the same variable
-			rem4 = [],
-			rem5 = [],
-			rem6 = [],
-
-		// subtract to find z
-			z1 = [],
-			z2 = [],
-			z3 = [],
-
-		// put z in to solve for y
-			y1 = [],
-
-		// put all the variables in to get x
-			x1 = [],
-
+		// array containing all the  numbers
+		var array = [
+			[
+				[], [], []
+			],
+			[
+				[], [], []
+			],
+			[
+				[], [], []
+			],
+			[], []
+		],
+			x,
+			y,
+			z,
 			showx,
 			showy,
 			showz,
-
 			resultx,
 			resulty,
 			resultz;
-
 		var firstP = function firstP () {
 			// get rid of x
 
-			// rem1.push(e1[0] * e2[0]);
-			rem1.push(e1[1] * e2[0]);
-			rem1.push(e1[2] * e2[0]);
-			rem1.push(e1[3] * e2[0]);
+			// array[0][0].push(e1[0] * e2[0]);
+			array[0][0].push(e1[1] * e2[0]);
+			array[0][0].push(e1[2] * e2[0]);
+			array[0][0].push(e1[3] * e2[0]);
 
-			// rem2.push(e2[0] * e1[0]);
-			rem2.push(e2[1] * e1[0]);
-			rem2.push(e2[2] * e1[0]);
-			rem2.push(e2[3] * e1[0]);
+			// array[0][1].push(e2[0] * e1[0]);
+			array[0][1].push(e2[1] * e1[0]);
+			array[0][1].push(e2[2] * e1[0]);
+			array[0][1].push(e2[3] * e1[0]);
 
-			rem3.push(rem1[0] - rem2[0]);
-			rem3.push(rem1[1] - rem2[1]);
-			rem3.push(rem1[2] - rem2[2]);
+			array[0][2].push(array[0][0][0] - array[0][1][0]);
+			array[0][2].push(array[0][0][1] - array[0][1][1]);
+			array[0][2].push(array[0][0][2] - array[0][1][2]);
 		};
 
 		var secondP = function secondP () {
 			// get rid of x
 
-			// rem4.push(e1[0] * e2[0]);
-			rem4.push(e1[1] * e3[0]);
-			rem4.push(e1[2] * e3[0]);
-			rem4.push(e1[3] * e3[0]);
+			// array[1][0].push(e1[0] * e2[0]);
+			array[1][0].push(e1[1] * e3[0]);
+			array[1][0].push(e1[2] * e3[0]);
+			array[1][0].push(e1[3] * e3[0]);
 
-			// rem5.push(e3[0] * e1[0]);
-			rem5.push(e3[1] * e1[0]);
-			rem5.push(e3[2] * e1[0]);
-			rem5.push(e3[3] * e1[0]);
+			// array[1][1].push(e3[0] * e1[0]);
+			array[1][1].push(e3[1] * e1[0]);
+			array[1][1].push(e3[2] * e1[0]);
+			array[1][1].push(e3[3] * e1[0]);
 
-			rem6.push(rem4[0] - rem5[0]);
-			rem6.push(rem4[1] - rem5[1]);
-			rem6.push(rem4[2] - rem5[2]);
+			array[1][2].push(array[1][0][0] - array[1][1][0]);
+			array[1][2].push(array[1][0][1] - array[1][1][1]);
+			array[1][2].push(array[1][0][2] - array[1][1][2]);
 		};
 
 		firstP();
 		secondP();
 
-		z1.push(rem3[0] * rem6[0]);
-		z1.push(rem3[1] * rem6[0]);
-		z1.push(rem3[2] * rem6[0]);
+		array[2][0].push(array[0][2][0] * array[1][2][0]);
+		array[2][0].push(array[0][2][1] * array[1][2][0]);
+		array[2][0].push(array[0][2][2] * array[1][2][0]);
 
-		z2.push(rem6[0] * rem3[0]);
-		z2.push(rem6[1] * rem3[0]);
-		z2.push(rem6[2] * rem3[0]);
+		array[2][1].push(array[1][2][0] * array[0][2][0]);
+		array[2][1].push(array[1][2][1] * array[0][2][0]);
+		array[2][1].push(array[1][2][2] * array[0][2][0]);
 
 		// f3.push(f1[0] - f2[0]);
-		z3.push(z1[1] - z2[1]);
-		z3.push(z1[2] - z2[2]);
+		array[2][2].push(array[2][0][1] - array[2][1][1]);
+		array[2][2].push(array[2][0][2] - array[2][1][2]);
 
 		// solve for z
-		z = z3[1] / z3[0];
+		z = array[2][2][1] / array[2][2][0];
 
-		y1.push(rem3[0]);
-		y1.push(rem3[1] * z);
-		y1.push(rem3[2]);
+		array[3].push(array[0][2][0]);
+		array[3].push(array[0][2][1] * z);
+		array[3].push(array[0][2][2]);
 
 		// solve for y
-		y = (y1[2] - y1[1]) / y1[0];
+		y = (array[3][2] - array[3][1]) / array[3][0];
 
 		// use y and z to find x
-		x1.push(e1[0]);
-		x1.push(e1[1] * y);
-		x1.push(e1[2] * z);
-		x1.push(e1[3]);
+		array[4].push(e1[0]);
+		array[4].push(e1[1] * y);
+		array[4].push(e1[2] * z);
+		array[4].push(e1[3]);
 
 		// solve for x
-		x = (x1[3] - (x1[1] + x1[2])) / x1[0];
+		x = (array[4][3] - (array[4][1] + array[4][2])) / array[4][0];
+
+		// shows the answer in {(x,y,z)}
+		console.log("Answer: {(" + x + ", " + y + ", " + z + ")}");
 
 		showx = new Fraction(x);
 		resultx = showx.toFraction(true);
@@ -160,6 +154,7 @@ function threeEquations (e1, e2, e3) {
 		console.log(e1[0] * x + e1[1] * y + e1[2] * z);
 		console.log(e2[0] * x + e2[1] * y + e2[2] * z);
 		console.log(e3[0] * x + e3[1] * y + e3[2] * z);
+
 	};
 	solve(e1, e2, e3);
 	useTwo = false;
